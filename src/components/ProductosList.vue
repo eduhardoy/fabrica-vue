7ab1077a2769846dfc8f63614ce4ac4b23dc9dfc
Edu Hardoy <eduhardoy@gmail.com>
2021-03-23 20:39:18 -0300

    ProductsList

--- src/components/ProductosList.vue
+++ src/components/ProductosList.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="products_head">
-    <div class="products_head_title"></div>
-    <div class="products_head_adds"></div>
-    <div class="products_head_filters"></div>
+  <div class="products">
+    <div class="products_head">
+      <div class="products_head_title">
+        <h2>PRODUCTOS</h2>
+      </div>
+      <div class="products_head_adds">
+        <h2>PRODUCTOS</h2>
+      </div>
+      <div class="products_head_filters">
+        <h2>PRODUCTOS</h2>
+      </div>
+    </div>
+    <div class="products_accordion_wrapper"><h2>PRODUCTOS</h2></div>
   </div>
-  <div class="products_accordion_wrapper"></div>
 </template>
 
 <script>
@@ -13,4 +21,35 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.products {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.products_head {
+  height: 35%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  .products_head_title {
+    height: calc(100% / 3);
+  }
+  .products_head_adds {
+    height: calc(100% / 3);
+  }
+  .products_head_filters {
+    height: calc(100% / 3);
+  }
+}
+.products_accordion_wrapper {
+  height: 65%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
