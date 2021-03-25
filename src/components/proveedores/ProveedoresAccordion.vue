5b89f14630ae3d137c7cb84d382f29b63e77108f
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2021-03-25 13:02:51 -0300

    Proveedores falta estilo

--- src/components/proveedores/ProveedoresAccordion.vue
+++ src/components/proveedores/ProveedoresAccordion.vue
@@ -1,30 +1,143 @@
 <template>
   <div class="accordion__container">
-    <details class="accordion__box">
+    <details
+      class="accordion__box"
+      v-for="item in proveedores"
+      v-bind:key="item.id"
+    >
       <summary>
         <div class="summary__title">
-          <h3>DSASDADSAD</h3>
+          <h3>{{ item.NOMBRE }}</h3>
         </div>
         <div class="summary__buttons">
-          <button><img src="./images/pencil.svg" alt="" />EDITAR</button>
-          <button><img src="./images/trash.svg" alt="" />ELIMINAR</button>
+          <button @click="openEditModal(item)">
+            <img src="./images/pencil.svg" alt="" />EDITAR
+          </button>
+          <button @click="openDelModal(item)">
+            <img src="./images/trash.svg" alt="" />ELIMINAR
+          </button>
         </div>
       </summary>
       <ul>
-        <li>Email: Proveedor1@inibot.com</li>
-        <li>Email: Proveedor1@inibot.com</li>
-        <li>Email: Proveedor1@inibot.com</li>
-        <li>Email: Proveedor1@inibot.com</li>
-        <li>Email: Proveedor1@inibot.com</li>
-        <li>Email: Proveedor1@inibot.com</li>
+        <li>Direccion: {{ item.DIRECCION }}</li>
+        <li>Telefono: {{ item.TELEFONO }}</li>
+        <li>Email: {{ item.EMAIL }}</li>
+        <li>CUIT: {{ item.CUIT }}</li>
+        <ul v-for="item2 in item.CUENTA_BANCO" v-bind:key="item2.id">
+          <li>CBU: {{ item2.CBU }}</li>
+          <li>BANCO: {{ item2.BANCO }}</li>
+        </ul>
       </ul>
     </details>
   </div>
+  <ModalDelete ref="del">
+    <template v-slot:body>
+      <p>Esta seguro que desea eliminar?</p>
+    </template>
+    <template v-slot:footer>
+      <button class="cancel_button" @click="$refs.del.closeModal()">
+        CANCELAR
+      </button>
+      <button
+        class="delete_button"
+        @click="deleteProveedor(this.selectedProveedor)"
+      >
+        ELIMINAR
+      </button>
+    </template>
+  </ModalDelete>
+  <ModalEdit ref="edit">
+    <template v-slot:body>
+      <input v-model="selectedProveedor.NOMBRE" placeholder="NOMBRE" />
+      <input v-model="selectedProveedor.DIRECCION" placeholder="DIRECCION" />
+      <input v-model="selectedProveedor.TELEFONO" placeholder="TELEFONO" />
+      <input v-model="selectedProveedor.EMAIL" placeholder="EMAIL" />
+      <input v-model="selectedProveedor.CUIT" placeholder="CUIT" />
+
+      <div
+        v-for="item in selectedProveedor.CUENTA_BANCO"
+        :key="item.CBU"
+        class="cbu"
+      >
+        <input v-model.lazy="item.CBU" placeholder="CBU" />
+        <input v-model.lazy="item.BANCO" placeholder="BANCO" />
+      </div>
+      <div>
+        <button
+          v-if="selectedProveedor.CUENTA_BANCO.length > 1"
+          @click="delCBU(selectedProveedor)"
+        >
+          DEL CBU
+        </button>
+        <button @click="addCBU(selectedProveedor)">ADD CBU</button>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <button class="cancel_button" @click="$refs.change.closeModal()">
+        CANCELAR
+      </button>
+      <button
+        class="change_button"
+        @click="putProveedor(this.selectedProveedor)"
+      >
+        MODIFICAR
+      </button>
+    </template>
+  </ModalEdit>
 </template>
 
 <script>
+import ModalDelete from "../Modals/ModalDelete.vue";
+import ModalEdit from "../Modals/ModalChange.vue";
+
 export default {
   name: "ProveedoresAccordion",
+  components: { ModalDelete, ModalEdit },
+  data() {
+    return {
+      selectedProveedor: {},
+    };
+  },
+  computed: {
+    proveedores: function () {
+      return this.$store.getters.allProveedores;
+    },
+  },
+  methods: {
+    openDelModal: function (proveedor) {
+      this.selectedProveedor = proveedor;
+      this.$refs.del.openModal();
+    },
+    openEditModal: function (proveedor) {
+      this.selectedProveedor = proveedor;
+      this.$refs.edit.openModal();
+    },
+    addCBU: function (variable) {
+      variable.CUENTA_BANCO.push({ value: "" });
+    },
+    delCBU: function (variable) {
+      variable.CUENTA_BANCO.pop();
+    },
+    getProveedores: function () {
+      this.$store.dispatch("getProveedores");
+      console.log("Proveedores", this.$store.getters.allProveedores);
+    },
+    postProveedor: function () {
+      this.$store.dispatch("postProveedor", this.newProveedor);
+      this.$refs.add.closeModal();
+    },
+    putProveedor: function (proveedor) {
+      this.$store.dispatch("putProveedor", proveedor);
+      this.$refs.edit.closeModal();
+    },
+    deleteProveedor: function (proveedor) {
+      this.$store.dispatch("deleteProveedor", proveedor);
+      this.$refs.del.closeModal();
+    },
+  },
+  mounted() {
+    this.getProveedores();
+  },
 };
 </script>
 
--- src/components/proveedores/ProveedoresList.vue
+++ src/components/proveedores/ProveedoresList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="proveedores">
-    <button class="add__button">
+    <button class="add__button" @click="openAddModal">
       <img src="./images/plus.svg" alt="" />
     </button>
     <div class="proveedores__head">
@@ -12,11 +12,69 @@
       <slot></slot>
     </div>
   </div>
+  <ModalAdd ref="add">
+    <template v-slot:body>
+      <input v-model="newProveedor.NOMBRE" placeholder="NOMBRE" />
+      <input v-model="newProveedor.DIRECCION" placeholder="DIRECCION" />
+      <input v-model="newProveedor.TELEFONO" placeholder="TELEFONO" />
+      <input v-model="newProveedor.EMAIL" placeholder="EMAIL" />
+      <input v-model="newProveedor.CUIT" placeholder="CUIT" />
+
+      <div
+        v-for="item in newProveedor.CUENTA_BANCO"
+        :key="item.value"
+        class="cbu"
+      >
+        <input v-model.lazy="item.CBU" placeholder="CBU" />
+        <input v-model.lazy="item.BANCO" placeholder="BANCO" />
+      </div>
+      <div>
+        <button
+          v-if="newProveedor.CUENTA_BANCO.length > 1"
+          @click="delCBU(newProveedor)"
+        >
+          DEL CBU
+        </button>
+        <button @click="addCBU(newProveedor)">ADD CBU</button>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <button class="cancel_button" @click="$refs.add.closeModal()">
+        CANCELAR
+      </button>
+      <button class="add_button" @click="postProveedor()">AGREGAR</button>
+    </template>
+  </ModalAdd>
 </template>
 
 <script>
+import ModalAdd from "../Modals/ModalAdd.vue";
+
 export default {
   name: "ProveedoresList",
+  components: { ModalAdd },
+  data() {
+    return {
+      newProveedor: {
+        CUENTA_BANCO: [{ CBU: "", BANCO: "" }],
+      },
+    };
+  },
+  methods: {
+    openAddModal: function () {
+      this.$refs.add.openModal();
+    },
+    addCBU: function (variable) {
+      variable.CUENTA_BANCO.push({ value: "" });
+    },
+    delCBU: function (variable) {
+      variable.CUENTA_BANCO.pop();
+    },
+    postProveedor: function () {
+      this.$store.dispatch("postProveedor", this.newProveedor);
+      this.$refs.add.closeModal();
+    },
+  },
 };
 </script>
 
