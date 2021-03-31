fe2650264d5ebdfa160a95d3f26e8e839bb35932
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2021-03-30 21:56:20 -0300

    Productos in progress

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex'
 import proveedores from './proveedores/proveedores'
 import categorias from './categorias/categorias'
+import productos from './productos/productos'
 
 export default createStore({
   modules: {
     proveedores,
-    categorias
+    categorias,
+    productos
   }
 })
---
+++ src/store/productos/productos.js
@@ -0,0 +1,47 @@
+import Axios from "axios";
+
+const URL = "http://190.106.132.208:3005/productos";
+
+export default {
+    state: {
+        productos: [],
+        loading: false,
+    },
+    getters: {
+        allProductos: state => state.productos
+    },
+    actions: {
+        setProductosLoader({ commit }, loader) {
+            commit("SET_PRODUCTOS_LOADER", loader)
+        },
+        getProductos({ commit, dispatch }) {
+            dispatch("setProductosLoader", true)
+            Axios.get(URL)
+                .then(result => commit("SET_PRODUCTOS", result.data))
+                .catch(err => console.log(err))
+                .finally(() => dispatch("setProductosLoader", false))
+        },
+        async postProducto({ dispatch }, producto) {
+            dispatch("setProductosLoader", true)
+            Axios.post(URL, producto)
+                .then(() => dispatch("getProductos"))
+                .catch(err => console.log(err))
+                .finally(() => dispatch("setProductosLoader", false))
+        },
+        async deleteProducto({ dispatch }, producto) {
+            dispatch("setProductosLoader", true)
+            Axios.delete(URL + `/${producto._key}`)
+                .then(() => dispatch("getProductos"))
+                .catch(err => console.log(err))
+                .finally(() => dispatch("setProductosLoader", true))
+        }
+    },
+    mutations: {
+        SET_PRODUCTOS(state, productos) {
+            state.productos = productos;
+        },
+        SET_PRODUCTOS_LOADER(state, loader) {
+            state.loader = loader;
+        }
+    },
+};
